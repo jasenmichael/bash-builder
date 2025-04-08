80f6d08a69799e83b5f90a8b1dbc45ba7eb840f1
jasenmichael <jasen@jasenmichael.com>
2025-04-08 10:51:01 -0500

    feat: add remove_blocks.sh

---
+++ src/lib/remove_blocks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# =================================================================
+# Script: remove_blocks.sh
+# URL: https://github.com/jasenmichael/bash-builder/src/lib/remove_blocks.sh
+# !! Description: Removes specified code blocks from the script
+# !! Author: jasenmichael
+# !! License: MIT License
+#
+# Functions:
+# - remove_blocks: Removes specified code blocks from the script
+# =================================================================
+# ## shellcheck disable=SC2120
+remove_blocks() {
+  local start_line=${1:-"bash_builder_remove_start"}
+  local end_line=${2:-"bash_builder_remove_end"}
+  sed -i -e "/# ${start_line}/,/# ${end_line}/d" "$DEST"
+}
