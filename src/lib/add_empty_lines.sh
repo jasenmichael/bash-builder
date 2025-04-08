ea871a030c479630f90d0949c423ad7f1c649b3a
jasenmichael <jasen@jasenmichael.com>
2025-04-08 10:51:01 -0500

    feat: add add_empty_lines.sh

---
+++ src/lib/add_empty_lines.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# =================================================================
+# Script: add_empty_lines.sh
+# URL: https://github.com/jasenmichael/bash-builder/src/lib/add_empty_lines.sh
+# !! Description: Adds empty lines between code blocks to improve readability
+# !! Author: jasenmichael
+# !! License: MIT License
+#
+# Functions:
+# - add_empty_lines: Adds empty lines after lines containing only "}"
+# =================================================================
+add_empty_lines() {
+  # add empty line after lines containing only "}"
+  # shellcheck disable=SC1003
+  sed -i -e '/^[[:space:]]*}$/a\\' "$DEST"
+}
